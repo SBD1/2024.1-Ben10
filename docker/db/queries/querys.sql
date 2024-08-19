8493cb997d7c6b32a1cc1764d133c361975618f8
Eric Silveira <silveirames@hotmail.com>
2024-08-19 15:03:11 -0300

    inserindo querys de consultas do banco

--- docker/db/queries/querys.sql
+++ docker/db/queries/querys.sql
@@ -1,101 +1,176 @@
 -- =========================================== MISSÃO ===================================================
 
 -- Consultar os detalhes de todas as missões associadas a um determinado NPC
-SELECT m.id_missao, m.nome_missao, m.experiencia, m.descricao, m.recompensa_em_moedas
-FROM npc gdm
-JOIN missao m ON gdm.id_missao_associada = m.id_missao;
+select n.id_npc, m.*
+from instancia_npc_na_sala inns 
+join npc n ON inns.id_npc = n.id_npc
+join missao m on m.id_missao = n.id_missao_associada
+where inns.id_sala = 2;
+
+
+-- Consultando todos os itens e valores e diálogo associado ao estoque de um NPC
+select edi.id_npc, n.dialogo_associado_venda, edi.nome_item, edi.preco
+from instancia_npc_na_sala inns 
+join npc n ON inns.id_npc = n.id_npc
+join estoque_do_item edi on edi.id_npc = inns.id_npc
+where inns.id_sala = 12;
+
 
 -- SELECIONANDO MISSÕES CONCLUÍDAS PELO PERSONAGEM
 SELECT p.id_personagem, rdm.id_missao, rdm.status
 FROM personagem p
 JOIN registro_da_missao rdm ON p.id_personagem = rdm.id_personagem
-WHERE rdm.status ='completa';
+WHERE rdm.status ='completa' and rdm.id_personagem = 2;
 
 -- SELECIONANDO MISSÕES QUE O PERSONAGEM AINDA NÃO FEZ
 SELECT m.id_missao
 FROM missao m
-EXCEPT
+except
 SELECT rdm.id_missao
 FROM registro_da_missao rdm
-JOIN personagem p ON p.id_personagem = rdm.id_personagem AND rdm.status = 'completa';
+join personagem p on p.id_personagem = rdm.id_personagem and rdm.status = 'completa'
+where p.id_personagem = 2;
 
+-- Verificando os pré requisitos para uma missão específica
+select m.id_missao, m.nome_missao, pr.id_pre_requisito 
+from missao m
+join pre_requisito pr ON m.id_missao = pr.id_missao
+where m.id_missao = 5;
 
--- Verificando os pré-requisitos para realizar uma missão que o personagem fez
-SELECT pr.id_pre_requisito 
-FROM personagem p, registro_da_missao rdm, missao m, pre_requisito pr
-WHERE p.id_personagem = rdm.id_personagem AND rdm.id_missao <> m.id_missao;
+-- Verificando quais missões aquela missão desbloqueia
+select 
+	pr.id_missao as missao_posterior,
+	pr.id_pre_requisito as id_missao,
+	m.nome_missao
+from missao m
+join pre_requisito pr on pr.id_pre_requisito = m.id_missao
+where m.id_missao = 3;
 
 
 -- ============================================= PERSONAGEM =============================================
 
 -- Verificando a capacidade do iventário
-SELECT count(i.id_personagem) 
-FROM personagem p
-JOIN inventario i ON i.id_personagem = p.id_personagem
-WHERE i.id_personagem = 3;
+select count(i.id_personagem) 
+from personagem p
+join inventario i on i.id_personagem = p.id_personagem
+where i.id_personagem = 3;
+
+-- Verificando todos os itens do iventário
+select i.nome_item 
+from personagem p
+join inventario i on i.id_personagem = p.id_personagem
+where i.id_personagem = 3;
+
+-- Verificando todos os aliens de um personagem
+select sda.nome_alien
+from personagem p
+join status_do_alien sda on sda.id_personagem = p.id_personagem
+where sda.id_personagem = 3;
 
 -- Nome do personagem
-SELECT nome
-FROM personagem p
-WHERE p.id_personagem = 3;
+select nome
+from personagem p
+where p.id_personagem = 3;
 
 -- Nivel do personagem
-SELECT nivel
-FROM personagem p
-WHERE p.id_personagem = 3;
+select nivel
+from personagem p
+where p.id_personagem = 3;
 
 -- Saude do personagem
-SELECT saude
-FROM personagem p
-WHERE p.id_personagem = 3;
+select saude
+from personagem p
+where p.id_personagem = 3;
 
 -- Quantidade de moedas do personagem
-SELECT quantidade_moedas
-FROM personagem p
-WHERE p.id_personagem = 3;
+select quantidade_moedas
+from personagem p
+where p.id_personagem = 3;
 
 
 -- Recompensa relacionada a um personagem específico
-SELECT r.*
-FROM personagem p
-JOIN recompensa r ON r.id_personagem = p.id_personagem
-WHERE p.id_personagem = 3;
+select r.*
+from personagem p
+join recompensa r on r.id_personagem = p.id_personagem
+where p.id_personagem = 3;
 
 
-
--- ============================================ SALA ======================================
+-- ============================================ SALAS E REGIÕES ======================================
 
 -- Listar um npc em uma sala
-SELECT inns.id_npc, inns.id_sala
-FROM instancia_npc_na_sala inns
-JOIN sala s ON s.id_sala = inns.id_sala
-WHERE s.id_sala = 3;
+select inns.id_npc, inns.id_sala
+from instancia_npc_na_sala inns
+join sala s on s.id_sala = inns.id_sala
+where s.id_sala = 3;
 
--- Listar todas as zonas de guerra e sua dificuldade
-SELECT z.id_sala, z.dificuldade, s.nome_regiao
-FROM zona_de_guerra z
-JOIN sala s ON s.id_sala = z.id_sala;
+-- Listar todas as zonas de guerra de uma região e sua dificuldade
+SELECT zg.id_sala, zg.dificuldade, s.nome_regiao
+FROM zona_de_guerra zg
+JOIN sala s ON s.id_sala = zg.id_sala
+join regiao r on r.nome_regiao = s.nome_regiao
+where s.tipo_sala = 'Zona de Guerra';
+
+-- Listar todas as zonas de armadilha de uma região
+SELECT zda.*, s.nome_regiao
+FROM zona_de_armadilha zda 
+JOIN sala s ON s.id_sala = zda.id_sala
+join regiao r on r.nome_regiao = s.nome_regiao
+where s.tipo_sala = 'Zona de Armadilha';
+
+-- listar todas as regiões
+select *
+from regiao r;
+
+-- listar todas as regiões e suas salas
+SELECT r.*, s.id_sala, s.id_pre_req_missao, s.tipo_sala 
+FROM regiao r
+join sala s on r.nome_regiao = s.nome_regiao;
+
 
 -- Listar todas as regiões
-SELECT r.*
-FROM regiao r;
+select r.*
+from regiao r;
 
 -- Listar todas as salas de uma determinada região, com seus pré requisitos
-SELECT r.nome_regiao, s.id_sala, s.id_pre_req_missao
-FROM sala s
-JOIN regiao r ON s.nome_regiao = r.nome_regiao
-WHERE r.nome_regiao = 'Nave do Vilgax';
+select r.nome_regiao, s.id_sala, s.id_pre_req_missao
+from sala s
+join regiao r on s.nome_regiao = r.nome_regiao
+where r.nome_regiao = 'Nave do Vilgax';
 
 -- Lista as salas que não tem pre-requisito
 SELECT s.id_sala
 FROM sala s
-JOIN registro_da_missao rdm ON s.id_pre_req_missao = rdm.id_missao
-JOIN personagem p ON rdm.id_personagem = p.id_personagem
-WHERE p.id_personagem = 3 and rdm.status = 'completa'
-UNION
-SELECT s.id_sala 
+join registro_da_missao rdm ON s.id_pre_req_missao = rdm.id_missao
+join personagem p on rdm.id_personagem = p.id_personagem
+where p.id_personagem = 3 and rdm.status = 'completa'
+union 
+select s.id_sala 
+from sala s
+WHERE s.id_pre_req_missao ISNULL;
+
+-- Buscando o log de uma determinada sala
+select izg.*, zdg.descricao
+from zona_de_guerra zdg
+join instancia_zona_guerra izg on izg.id_zona_guerra = zdg.id_sala
+where zdg.id_sala = 3;
+
+-- Buscando o log de um personagem específico
+select izg.*
+from personagem p
+join instancia_zona_guerra izg on izg.id_zona_guerra = p.id_sala
+where izg.id_personagem = 1;
+
+-- Buscando detalhes da recompensa na zona de armadilha
+select r.*
+from zona_de_armadilha zda
+join recompensa r on r.id_sala = zda.id_sala
+where r.id_sala = 3;
+
+-- Verificar qual sala está desbloqueada para um personagem
+SELECT s.*
 FROM sala s
-WHERE s.id_pre_req_missao IS NULL;
+LEFT JOIN registro_da_missao rdm ON s.id_pre_req_missao = rdm.id_missao AND rdm.id_personagem = 2 AND rdm.status = 'completa'
+WHERE rdm.id_missao IS NOT NULL OR s.id_pre_req_missao IS NULL;
 
 
 -- =========================================== MONSTRO ==================================
@@ -104,58 +179,59 @@ WHERE s.id_pre_req_missao IS NULL;
 SELECT izg.id_zona_guerra, im.id_monstro, im.nome_especie, im.saude_atual
 FROM instancia_monstro im
 JOIN instancia_zona_guerra izg ON im.id_monstro = izg.id_monstro
-WHERE izg.id_zona_guerra = 3;
+where izg.id_zona_guerra = 3;
 
 -- Consultar dados especificos de uma espécie
-SELECT m.*, im.saude_atual 
-FROM monstro m
-JOIN instancia_monstro im ON im.nome_especie = m.nome;
+SELECT e.*, a.*, m.*
+FROM especie e
+LEFT JOIN alien a ON a.nome = e.nome AND e.tipo_especie = 'Alien'
+LEFT JOIN monstro m ON m.nome = e.nome AND e.tipo_especie = 'Monstro'
+WHERE e.nome = 'Insectóide';
+
+-- Consultar habilidades especificas de uma espécie
+SELECT e.*, h.*
+FROM especie e
+LEFT JOIN habilidade h ON h.nome_especie = e.nome
+WHERE e.nome = 'Insectóide';
+
 
 -- Monstro e Drop do item
-SELECT im.id_monstro, 
-       COALESCE(c.nome_item, a.nome_item) AS nome_item_recompensa
-FROM instancia_monstro im
-JOIN monstro m ON m.nome = im.nome_especie
-LEFT JOIN consumivel c ON m.id_recompensa = c.nome_item
-LEFT JOIN arma a ON m.id_recompensa = a.nome_item
-WHERE im.id_monstro = 3;
+SELECT m.nome, i.*
+from monstro m 
+join item i ON m.id_recompensa = i.nome_item
+where m.nome = 'Demônio';
 
 
 -- ========================================= ITEM ===============================
 
 -- Verificando os itens do inventário do personagem
-SELECT i.nome_item 
-FROM personagem p
-JOIN inventario i ON i.id_personagem = p.id_personagem
-WHERE i.id_personagem = 3;
+select i.nome_item 
+from personagem p
+join inventario i on i.id_personagem = p.id_personagem
+where i.id_personagem = 3;
 
 -- Verificando o valor de venda do item do personagem
-SELECT i.nome_item, COALESCE(c.valor_consumivel, a.preco) AS atributo
-FROM personagem p
-JOIN inventario i ON i.id_personagem = p.id_personagem
-LEFT JOIN consumivel c ON c.nome_item = i.nome_item
-LEFT JOIN arma a ON a.nome_item = i.nome_item;
-
--- Listar todos os itens disponíveis para venda por um NPC
-SELECT edi.nome_item, edi.preco
-FROM estoque_do_item edi
-JOIN npc vendedor ON edi.id_npc = vendedor.id_npc
-WHERE edi.id_npc = 6;
+select i.nome_item,  c.preco, a.preco 
+from personagem p
+left join inventario i on i.id_personagem = p.id_personagem
+left join item it on it.nome_item = i.nome_item
+left join consumivel c on c.nome_item = i.nome_item and it.tipo_item = 'Consumível'
+left join arma a on a.nome_item = i.nome_item and it.tipo_item = 'Arma'
+where p.id_personagem = 3;
 
 -- Consultar as recompensas de uma sala específica
 SELECT s.id_sala, r.nome_item, r.recompensa_recebida
 FROM recompensa r
-JOIN sala s ON s.id_sala = r.id_sala
-WHERE s.id_sala = 3;
+join sala s on s.id_sala = r.id_sala
+where s.id_sala = 3;
 
 -- Consultar qual a recompensa em forma de item
-SELECT r.nome_item, 
-       COALESCE(c.valor_consumivel, a.dano) AS atributo
+SELECT r.*, c.*, a.*
 FROM recompensa r
-JOIN estoque_do_item edi ON r.nome_item = edi.nome_item
-LEFT JOIN consumivel c ON c.nome_item = edi.nome_item 
-LEFT JOIN arma a ON a.nome_item = edi.nome_item
-WHERE edi.nome_item ='Kit Médico';
+join item i on r.nome_item = i.nome_item
+LEFT JOIN CONSUMIVEL c ON i.nome_item = c.nome_item AND i.tipo_item = 'Consumível'
+LEFT JOIN ARMA a ON i.nome_item = c.nome_item AND i.tipo_item = 'Arma'
+WHERE r.nome_item = 'Kit Médico';
 
 
 -- ========================================== ALIEN ==============================
@@ -163,12 +239,12 @@ WHERE edi.nome_item ='Kit Médico';
 -- Verificar o status de vida de um alien de um personagem
 SELECT p.id_personagem, sda.nome_alien, sda.saude
 FROM status_do_alien sda
-JOIN personagem p ON p.id_personagem = sda.id_personagem
-WHERE p.id_personagem = 3 and sda.nome_alien = 'XLR8';
+join personagem p on p.id_personagem = sda.id_personagem
+where p.id_personagem = 3 and sda.nome_alien = 'XLR8';
 
 -- Verificar os detalhes do alien dos aliens dos personagens
-SELECT a.*
-FROM status_do_alien sda
-JOIN personagem p ON p.id_personagem = sda.id_personagem
-LEFT JOIN alien a ON a.nome = sda.nome_alien
-WHERE p.id_personagem = 3;
+select a.*
+from status_do_alien sda
+join personagem p on p.id_personagem = sda.id_personagem
+left join alien a on a.nome = sda.nome_alien
+where p.id_personagem = 3;
