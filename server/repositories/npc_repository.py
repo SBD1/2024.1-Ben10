bbbb6589bfb527c41017740539bc5fcc02d131ad
Joao Artur Leles <jalxpinheiro@gmail.com>
2024-08-22 18:51:36 -0300

    adicionando npc_repository

---
+++ server/repositories/npc_repository.py
@@ -0,0 +1,22 @@
+from database.db import create_connection
+
+def verificar_npc_na_sala(id_sala):
+
+    # Retorna o NPC atrelado à sala com o ID especificado.
+
+    try:
+        connection = create_connection(silent=True)
+        cursor = connection.cursor()
+        query = """
+            SELECT id_npc 
+            FROM INSTANCIA_NPC_NA_SALA
+            WHERE id_sala = %s
+        """
+        cursor.execute(query, id_sala)
+        npc = cursor.fetchone()  # Pega o npc associado a sala
+        cursor.close()
+        connection.close()
+        return npc  # Retorna o NPC ou None caso não exista
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
