83c6512839ee327da2dedf47ad1150345bc6631d
Joao Artur Leles <jalxpinheiro@gmail.com>
2024-09-06 20:54:39 -0300

    alterando atributo de missao

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -22,7 +22,7 @@ CREATE TABLE REGIAO (
 
 CREATE TABLE MISSAO (
     id_missao INT PRIMARY KEY,
-    nome_missao VARCHAR(30) NOT NULL,
+    nome_missao TEXT NOT NULL,
     experiencia INT NOT NULL,
     descricao TEXT NOT NULL,
     recompensa_em_moedas INT NOT NULL,
