7cd7879a3bf8b231ed62cd3e9cf73722b9b575c3
Rodolfo Cabral Neves <roddas360@gmail.com>
2024-09-08 09:18:11 -0300

    Atualizar o tipo efeito_armadilha

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,4 +1,13 @@
-CREATE TYPE tipo_reducao_acrescimo AS ENUM ('redução', 'acréscimo');
+CREATE TYPE efeito_armadilha AS ENUM (
+    'reducao_dano_arma_metade',
+    'reducao_dano_arma',
+    'reducao_vida_metade',
+    'reducao_vida',
+    'remocao_armadura',
+    'remocao_camuflagem',
+    'remocao_jato'
+);
+
 CREATE TYPE tipo_status AS ENUM ('buff_dano', 'critico', 'imunidade', 'vida_extra', 'cura');
 CREATE TYPE tipo_status_missao AS ENUM ('completa', 'em progresso');
 CREATE TYPE tipo_habilidade AS ENUM ('dano', 'cura');
